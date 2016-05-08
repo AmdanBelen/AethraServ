b503e763c1a17c0cbff40b04a37cbaac7f065749
Soramdan <cedric.hoelzl@gmail.com>
2016-05-08 18:13:07 +0200

    WIP

--- app.js
+++ app.js
@@ -6,18 +6,35 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+var stylus = require('stylus');
+var nib =  nib = require('nib');
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
+app.use(stylus.middleware({
+  src: __dirname + '/ressources',
+  dest: __dirname + '/public',
+  compile:compile
+}));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
-app.use('/', routes);
+app.get('/', function (req, res) {
+  res.render('homepage', { title: 'Aethra', message: 'Aethra'});
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -25,23 +42,10 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
-//================
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send('error'+ err.message );
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error'+ err.message );
+  //res.send('Error: '+ err.message );
+  res.render('error.pug',{status:err.status,message:err.message})
 });
 
 
--- server.js
+++ server.js
@@ -3,31 +3,17 @@
 var AppContainer = function () {
   //  Scope.
   var self = this;
-  /*  ================================================================  */
-  /*  Helper functions.                                                 */
-  /*  ================================================================  */
 
-  /**
-   *  Set up server IP address and port # using env variables/defaults.
-   */
   self.setupVariables = function () {
-    //  Set the environment variables we need.
     self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
     self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
     if (typeof self.ipaddress === "undefined") {
-      //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-      //  allows us to run/test the app locally.
-      console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
       self.ipaddress = "127.0.0.1";
     }
   };
 
-  /**
-   *  terminator === the termination handler
-   *  Terminate server on receipt of the specified signal.
-   *  @param {string} sig  Signal to terminate on.
-   */
+
   self.terminator = function (sig) {
     if (typeof sig === "string") {
       console.log('%s: Received %s - terminating sample app ...',
@@ -38,9 +24,6 @@ var AppContainer = function () {
   };
 
 
-  /**
-   *  Setup termination handlers (for exit and a list of signals).
-   */
   self.setupTerminationHandlers = function () {
     //  Process on exit and signals.
     process.on('exit', function () {

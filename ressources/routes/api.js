20f86ea177233ce283fca40eb45ee638318e196b
Soramdan <cedric.hoelzl@gmail.com>
2016-05-16 19:28:44 +0200

    WIP

--- ressources/routes/api.js
+++ ressources/routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/account');
+var bCrypt = require('bcrypt-nodejs');
 
 var isAuthenticated = function (req, res, next) {
   // if user is authenticated in the session, call the next() to call the next request handler 

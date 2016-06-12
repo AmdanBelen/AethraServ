712d4d133ad7dd8a2bb71ba00bbb37bd493a6c7c
Cédric Hoelzl <cedric.hoelzl@gmail.com>
2016-06-12 12:28:30 +0200

    wip

---
+++ ressources/routes/sub.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var isAuthenticated = function (req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler 
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  if (req.isAuthenticated())
+    return next();
+  // if the user is not authenticated then redirect him to the login page
+  res.redirect('/');
+}
+
+module.exports = function(passport){
+
+  router.use(function (req, res, next) {
+      res.locals.LoginMessage = req.flash('message');
+      res.json({"hi":"hi"});
+  });
+
+  return router;
+}
